8bfa26d436866aa5b3f1163ef79bb971099fbab2
Volodymyr Viter <mad.tvido@gmail.com>
2022-05-08 12:33:03 +0300

    upd

--- routes/api/candidates/candidates.js
+++ routes/api/candidates/candidates.js
@@ -4,10 +4,10 @@ const ctrl = require('../../../controllers/candidates');
 const {validation, controllerWrapper, authenticate} = require("../../../middlewares");
 const {joiSchema} = require("../../../schemas/candidate")
 
-router.get('/', controllerWrapper(ctrl.getAllCandidates));
+router.get('/', controllerWrapper(authenticate), controllerWrapper(ctrl.getAllCandidates));
 router.post('/', controllerWrapper(authenticate), validation(joiSchema), controllerWrapper(ctrl.addCandidate));
-router.get('/:candidateId', controllerWrapper(ctrl.getCandidateById));
-router.delete('/:candidateId', controllerWrapper(ctrl.deleteCandidate));
-router.put('/:candidateId', validation(joiSchema), controllerWrapper(ctrl.update));
+router.get('/:candidateId', controllerWrapper(authenticate), controllerWrapper(ctrl.getCandidateById));
+router.delete('/:candidateId', controllerWrapper(authenticate), controllerWrapper(ctrl.deleteCandidate));
+router.put('/:candidateId', controllerWrapper(authenticate), validation(joiSchema), controllerWrapper(ctrl.update));
 
 module.exports = router;
