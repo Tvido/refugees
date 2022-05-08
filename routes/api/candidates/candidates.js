48f490f1560d78edcc33600f85df40b95dca36f5
Volodymyr Viter <mad.tvido@gmail.com>
2022-05-08 17:06:47 +0300

    upd

--- routes/api/candidates/candidates.js
+++ routes/api/candidates/candidates.js
@@ -4,8 +4,8 @@ const ctrl = require('../../../controllers/candidates');
 const {validation, controllerWrapper, authenticate} = require("../../../middlewares");
 const {joiSchema} = require("../../../schemas/candidate")
 
-router.get('/', controllerWrapper(ctrl.getAllCandidates));
-router.post('/', validation(joiSchema), controllerWrapper(ctrl.addCandidate));
+router.get('/', controllerWrapper(authenticate), controllerWrapper(ctrl.getAllCandidates));
+router.post('/', controllerWrapper(authenticate), validation(joiSchema), controllerWrapper(ctrl.addCandidate));
 router.get('/:candidateId', controllerWrapper(authenticate), controllerWrapper(ctrl.getCandidateById));
 router.delete('/:candidateId', controllerWrapper(authenticate), controllerWrapper(ctrl.deleteCandidate));
 router.put('/:candidateId', controllerWrapper(authenticate), validation(joiSchema), controllerWrapper(ctrl.update));
