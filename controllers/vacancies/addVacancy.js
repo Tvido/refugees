9662389b5a60ad073f88d45ab843e7b3cae7e7ef
Volodymyr Viter <mad.tvido@gmail.com>
2022-05-12 13:46:36 +0300

    push

--- controllers/vacancies/addVacancy.js
+++ controllers/vacancies/addVacancy.js
@@ -3,6 +3,7 @@ const {Vacancy} = require("../../schemas");
 const addVacancy = async (req, res, next) => {
   try {
     const result = await Vacancy.create(req.body);
+    console.log('result', result)
     res.status(201).json({ result });
   } catch (error) {
     next(error);
