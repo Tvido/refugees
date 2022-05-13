9fc7a135d993ba7dcf6da5b38f21983d14f243e0
Volodymyr Viter <mad.tvido@gmail.com>
2022-05-13 13:58:12 +0300

    push

--- controllers/vacancies/addVacancy.js
+++ controllers/vacancies/addVacancy.js
@@ -1,13 +1,39 @@
-const {Vacancy} = require("../../schemas");
+const { Vacancy } = require("../../schemas");
 
 const addVacancy = async (req, res, next) => {
   try {
-    const result = await Vacancy.create(req.body);
-    console.log('result', result)
-    res.status(201).json({ result });
+    const candidate = await Vacancy.findOne({ owner: req.user._id })
+
+    if (candidate) {
+      return res.status(409).json({
+        status: "error",
+        code: 409,
+        message: "Resume already exist",
+      });
+    }
+
+    const newVacancy = { ...req.body, owner: req.user._id };
+    const result = await Vacancy.create(newVacancy);
+    res.status(201).json({
+      status: "success",
+      code: 201,
+      data: { result },
+    });
   } catch (error) {
     next(error);
   }
 };
 
 module.exports = addVacancy;
+
+// const addVacancy = async (req, res, next) => {
+//   try {
+//     const result = await Vacancy.create(req.body);
+//     console.log('result', result)
+//     res.status(201).json({ result });
+//   } catch (error) {
+//     next(error);
+//   }
+// };
+
+// module.exports = addVacancy;
--- controllers/vacancies/getAllVacancies.js
+++ controllers/vacancies/getAllVacancies.js
@@ -1,8 +1,12 @@
-const {Vacancy} = require("../../schemas");
+const { Vacancy } = require("../../schemas");
 
 const getAllVacancies = async (req, res, next) => {
   try {
-    const vacancies = await Vacancy.find({});
+    const vacancies = await Vacancy.find({ owner: req.user._id }).populate(
+      "owner",
+      "_id email",
+      "roles"
+    );
     res.json({
       vacancies,
     });
